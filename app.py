1460efc1cb1bf1a4904e16398570b264cd857f1d
ivankoros <ivan.koros2@gmail.com>
2023-03-26 15:51:57 -0400

    updated logic of app.py to text back name of sub deals when "sale" is in the body by querying the database for the current date

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask, Response, request
 from twilio.twiml.messaging_response import MessagingResponse
-import random
+from datetime import datetime
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from main import SubDeal  # Replace 'your_main_script' with the name of your main script file
 
 app = Flask(__name__)
 
+# Database configuration
+engine = create_engine('sqlite:///sub_deal_data.db')
+Session = sessionmaker(bind=engine)
+session = Session()
 
 @app.route("/sms", methods=['GET', 'POST'])
 def incoming_sms():
@@ -12,10 +20,16 @@ def incoming_sms():
     resp = MessagingResponse()
 
     if "sale" in body.lower():
-        sales = ["Italian Sub", "Ham Sub", "Turkey Sub", "Veggie Sub", "Tuna Sub", "Meatball Sub"]
-        resp.message("The " + random.choice(sales))
-        resp.message("The " + random.choice(sales))
-        resp.message("The " + random.choice(sales))
+        today = datetime.now().date()
+        print(today)
+
+        sales = session.query(SubDeal).filter(SubDeal.date == today).all()
+
+        if sales:
+            for sale in sales:
+                resp.message("The " + sale.name + " is on sale today.")
+        else:
+            resp.message("No subs on sale today.")
     else:
         resp.message("Sorry, I don't understand.")
 
