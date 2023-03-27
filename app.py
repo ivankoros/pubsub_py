39af72b30816e7c499f7e3af5b173c9168c7c16c
ivankoros <ivan.koros2@gmail.com>
2023-03-26 22:27:30 -0400

    set up mysql database in app.py and added better text responses based on the amount of deals in the current day (one vs multiple, giving different grammar responses)

--- app.py
+++ app.py
@@ -3,15 +3,16 @@ from twilio.twiml.messaging_response import MessagingResponse
 from datetime import datetime
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-from main import SubDeal  # Replace 'your_main_script' with the name of your main script file
+from main import SubDeal
+from main import initialize_database
+
+Base = declarative_base()
+
+session = initialize_database()
 
 app = Flask(__name__)
-
-# Database configuration
-engine = create_engine('sqlite:///sub_deal_data.db')
-Session = sessionmaker(bind=engine)
-session = Session()
 
 @app.route("/sms", methods=['GET', 'POST'])
 def incoming_sms():
@@ -25,13 +26,17 @@ def incoming_sms():
 
         sales = session.query(SubDeal).filter(SubDeal.date == today).all()
 
-        if sales:
+        if len(sales) == 1:
             for sale in sales:
-                resp.message("The " + sale.name + " is on sale today.")
+                resp.message("The " + sale.name.lower() + " is on sale today.")
+        elif len(sales) > 1:
+
+            resp.message(f"The {''.join([sale.name.lower() + ', ' for sale in sales])[:-2]} are on sale today")
         else:
             resp.message("No subs on sale today.")
     else:
         resp.message("Sorry, I don't understand.")
+        resp.message(f"Say 'sale' to see what subs are on sale today.")
 
     return Response(str(resp), mimetype="application/xml")
 
