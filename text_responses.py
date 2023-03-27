4ce61204b436b75c083cf176756e24c514ea9c60
ivankoros <ivan.koros2@gmail.com>
2023-03-26 22:34:13 -0400

    added a text responses class in text_responses.py for modularized identification of user text and reply prompts

---
+++ text_responses.py
@@ -0,0 +1,21 @@
+# These are the text responses that I'm using in a class
+
+class TextResponses:
+    def __init__(self):
+        self.text_responses = {
+            "help": ["Ask me 'any deals today?' to see what subs are on sale today.",
+                     "Ask 'what's on sale' to see what on sale today.",
+                     "Ask me what's on sale right now to get today's sub deals"],
+            "no_sale": "There are no subs on sale today.",
+            "error": "Sorry, I don't understand.",
+            "sale_prompt": ["deal",
+                            "sale",
+                            "on sale",
+                            "on sale today",
+                            "on sale right now",
+                            "today's deals",
+                            "today's deal"]
+        }
+
+    def get_response(self, response):
+        return self.text_responses[response]
