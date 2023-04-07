e173a30189ce4ee8d49a22b5094c9641dd4ab3ae
ivankoros <ivan.koros2@gmail.com>
2023-04-06 22:38:49 -0400

    updated naming of the customizations dictionary to not conflict with the customizations class attribute and updated its respective import statement

--- backend/twilio_app/__init__.py
+++ backend/twilio_app/__init__.py
@@ -1,5 +1,5 @@
 from backend.twilio_app.helpers import find_nearest_stores
-from backend.twilio_app.helpers import TextResponses, customizations
+from backend.twilio_app.helpers import TextResponses, all_customizations
 from backend.twilio_app.app import app
 from backend.twilio_app.state_flow_mechanism import (get_user, start_action, get_name_action, \
                                                      get_store_location_action, get_sale_action, default_action,
