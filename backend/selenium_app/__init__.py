921ec4c936427c97c5eaad1f148eb01b25faef85
ivankoros <ivan.koros2@gmail.com>
2023-04-07 17:28:49 -0400

    updated imports to specify where they're getting the helpers file from (because there are two) and deleted import of flow_mechanism files that was causing a circular import exception sometimes

--- backend/selenium_app/__init__.py
+++ backend/selenium_app/__init__.py
@@ -1,1 +1,1 @@
-from backend.selenium_app.order_sub_auto import order_sub
+
--- backend/twilio_app/__init__.py
+++ backend/twilio_app/__init__.py
@@ -1,6 +1,3 @@
 from backend.twilio_app.helpers import find_nearest_stores
 from backend.twilio_app.helpers import TextResponses, all_customizations
 from backend.twilio_app.app import app
-from backend.twilio_app.state_flow_mechanism import (get_user, start_action, get_name_action, \
-                                                     get_store_location_action, get_sale_action, default_action,
-                                                     state_info)
