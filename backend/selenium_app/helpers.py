a999fe4d0235845efa5dddc94cbaaabf5e6a2f97
ivankoros <ivan.koros2@gmail.com>
2023-04-06 22:37:32 -0400

    added in-depth docstring comments on the custom webdriver_location_input function and the properties I default the chromium browser to have that allows this

--- backend/selenium_app/helpers.py
+++ backend/selenium_app/helpers.py
@@ -4,6 +4,14 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 
 def webdriver_location_input(driver, location):
+    """Choosing store location
+
+    This code above is needed because we initially block our location services.
+    The input for choosing the location pops up automatically, so we click on the text box, enter an address, and press enter.
+    The store elements are easy to locate because each has their own unique labels (eg. "Choose St. John's Town Center as your store").
+    So, we use the address variable to find the correct store, wait for it to load, and then click on it. This exits the address prompt.
+
+    """
     location_selection_textbox_path = '//*[@id="navBar"]/div/div[2]/div/div/div[1]/div[2]/div/div/div[2]/div[2]/div/div/div/div[1]/form/input'
     address = location  # This is the address of the store we want to go to
     driver.find_element(By.XPATH, location_selection_textbox_path).send_keys(address)
