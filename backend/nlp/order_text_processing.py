38f6a8ee02cc1021a554a7c2c5f22b33504b2633
ivankoros <ivan.koros2@gmail.com>
2023-04-09 16:00:16 -0400

    created the find_synonyms function which takes a word and uses the NLTK wordnet library to find all unique lemma for that word, returning it as a list. this is for being able to match strings in the future based off their synonyms "hoagie" and "sandwich"

--- backend/nlp/order_text_processing.py
+++ backend/nlp/order_text_processing.py
@@ -16,3 +16,25 @@ def clean_text(user_text_input):
     text = " ".join([word for word in text.split() if word not in stop_words])
 
     return text
+
+def find_synonyms(word):
+    """Find synonyms of a word using WordNet
+
+    This function is for identifying the meaning of the user's input,
+    although they may not have used the correct words. For example,
+    if the user says a "meatball hoagie", we still want to be able to
+    find and return the "Publix Meatball Sub" sandwich.
+
+    This function takes in a word and follows this process:
+    1. Loop over each synonym (syn) in the synset (the group of synonyms) of the word.
+    2. Loop over each lemma (the base form of a word) in the synset.
+    3. For each lemma, add it to the set of synonyms.
+
+    :param word: string, single word
+    :return: list of synonyms for the given word
+    """
+    synonyms = set()
+    for syn in wordnet.synsets(word):
+        for lemma in syn.lemmas():
+            synonyms.add(lemma.name())
+    return list(synonyms)
